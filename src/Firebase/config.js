3ce8eac22300021d5964c9e0bf6bdd481219a586
Fasalu Rahman <128768240+faslkp@users.noreply.github.com>
2025-06-21 20:53:23 +0530

    Update config.js

--- src/Firebase/config.js
+++ src/Firebase/config.js
@@ -3,13 +3,13 @@ import { getFirestore } from "firebase/firestore";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDwZyllgzoYY68fbCyAwMzE-holtABUC9o",
-  authDomain: "test-f3c14.firebaseapp.com",
-  projectId: "test-f3c14",
-  storageBucket: "test-f3c14.firebasestorage.app",
-  messagingSenderId: "889462382273",
-  appId: "1:889462382273:web:fab9e61c52bcea59b8be40",
-  measurementId: "G-HPFNCXGW2N"
+  apiKey: "...",
+  authDomain: "...",
+  projectId: "...",
+  storageBucket: "...",
+  messagingSenderId: "...",
+  appId: "...",
+  measurementId: "..."
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
